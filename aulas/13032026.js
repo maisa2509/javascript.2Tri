b415de5e2b581d8f30ac07a45d8676d0cbc613d6
maisa2509 <maisa.silverio@escola.pr.gov.br>
2026-03-19 07:45:53 -0300

    create: Aula //19032026

--- aulas/13032026.js
+++ aulas/13032026.js
@@ -13,8 +13,23 @@ function resuldadoDivisaoSocio(lucro,lucro2){
 
 function calculadora(ari,a,b){
     switch(ari){
-        case `+´:
-            resultado = a+b;
+        case '+':
+            resultado = a + b;
             break;
         case '-':
+            resultado = a - b;
+            break;
+        case'*':
+            resultado = a * b ;
+            break;
+        case'/':
+            resultado = a / b;
+            break;
+        default:
+                resultado = ' Operador inválido
+
             
+    }
+    return resultado
+} 
+    
