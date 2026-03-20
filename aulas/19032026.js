34432941ae69785d3d46a9fecc81e8d3e528d022
maisa2509 <maisa.silverio@escola.pr.gov.br>
2026-03-20 07:53:54 -0300

    updent

--- aulas/19032026.js
+++ aulas/19032026.js
@@ -7,5 +7,27 @@ let numero = 30
 let decimal = 20.30
 // Sting
 
-let nome = "Não sei, você sabe?"
+let nome = "Não sei, você sabe?" 
+let nome1 = "NÃO SEI, VOCÊ NÃO SABE?"
+
+console.log(nome===nome); // false
+
+const inputMinusculo = nome1.toLowerCase();
+
+console.log(nome===inputMinusculo); //true
+
+const senha = "minhaSenha123"
+console.log(SVGLength) //13 caracteres
+
+
+//Booleanos
+
+let nv = true;
+let nz = false;
+
+if(nv!==nz)
+    console.log(`nv é diferente,pois ele è'${nv}`)
+}
+
+
 
